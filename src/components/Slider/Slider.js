de33a323144644b74385b4a05fa816a5d7992be8
Akif Dere <dereakif@gmail.com>
2021-03-28 17:04:25 +0300

    resposive button and fixed button text

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -42,7 +42,7 @@ const Slider = () => {
               insipidity age you simplicity understood. Do offering pleasure no
               ecstatic whatever on mr directly.
             </p>
-            <p className="read-more-btn">READ MORE</p>
+            <p className="read-more-btn">EXPLORE BRAND</p>
           </div>
         </div>
         <div className="carousel-item">
@@ -68,7 +68,7 @@ const Slider = () => {
               insipidity age you simplicity understood. Do offering pleasure no
               ecstatic whatever on mr directly.
             </p>
-            <p className="read-more-btn">READ MORE</p>
+            <p className="read-more-btn">EXPLORE SHOP</p>
           </div>
         </div>
       </div>
