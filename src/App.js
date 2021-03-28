9a35295d3297b21afb6bd96b5c9031e322ed72d3
Halık Ali TUNÇ <tunchalikali@gmail.com>
2021-03-28 15:14:10 +0300

    added news component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Brands from "./components/Brands/Brands";
 import FaceEyesLips from "./components/FaceEyesLips/FaceEyesLips";
 import NaturalQualityCashback from "./components/NaturalQualityCashback/NaturalQualityCashback";
+import News from "./components/News/News";
 import ShipSupportMoney from "./components/shipSupportMoney/ShipSupportMoney";
 import SkinCare from "./components/SkinCare/SkinCare";
 import Subscribe from "./components/Subscribe/Subscribe";
@@ -10,7 +11,7 @@ import TwoBlogsPart from "./components/TwoBlogsPart/TwoBlogsPart";
 function App() {
   return (
     <div className="App">
-      <TwoBlogsPart />
+      <News />
     </div>
   );
 }
