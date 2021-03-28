1df2e720b5690be820b95ddebd9daa30106de7cd
Züleyha Artan <53423158+ZuleyhaArtan@users.noreply.github.com>
2021-03-28 17:33:03 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+        
+      </Navbar>
 
     </div>
   );
