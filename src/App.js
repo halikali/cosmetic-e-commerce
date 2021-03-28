1d73ca586040ed20f14a734ddcec0c44e3b3c5a2
Halık Ali TUNÇ <tunchalikali@gmail.com>
2021-03-28 17:38:24 +0300

    added home pages with import

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import Slider from "./components/Slider/Slider";
-import Footer from "./components/Footer/Footer";
-import InstagramAndImages from "./components/InstagramAndImages/InstagramAndImages";
-import Brands from "./components/Brands/Brands";
-import FaceEyesLips from "./components/FaceEyesLips/FaceEyesLips";
-import NaturalQualityCashback from "./components/NaturalQualityCashback/NaturalQualityCashback";
-import News from "./components/News/News";
-import ShipSupportMoney from "./components/shipSupportMoney/ShipSupportMoney";
-import SkinCare from "./components/SkinCare/SkinCare";
-import Subscribe from "./components/Subscribe/Subscribe";
-import TwoBlogsPart from "./components/TwoBlogsPart/TwoBlogsPart";
+import Home from "./Pages/Home";
 import "./App.css";
-
 
 function App() {
   return (
     <div className="App">
-      <Slider />
-      <SkinCare />
-      <ShipSupportMoney />
-      <TwoBlogsPart />
-      <NaturalQualityCashback />
-      <FaceEyesLips />
-      <Subscribe />
-      <News />
-      <Brands />
-      <InstagramAndImages />
-      <Footer />
-
+      <Home />
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Slider from "../components/Slider/Slider";
+import Footer from "../components/Footer/Footer";
+import InstagramAndImages from "../components/InstagramAndImages/InstagramAndImages";
+import Brands from "../components/Brands/Brands";
+import FaceEyesLips from "../components/FaceEyesLips/FaceEyesLips";
+import NaturalQualityCashback from "../components/NaturalQualityCashback/NaturalQualityCashback";
+import News from "../components/News/News";
+import ShipSupportMoney from "../components/shipSupportMoney/ShipSupportMoney";
+import SkinCare from "../components/SkinCare/SkinCare";
+import Subscribe from "../components/Subscribe/Subscribe";
+import TwoBlogsPart from "../components/TwoBlogsPart/TwoBlogsPart";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <Slider />
+      <SkinCare />
+      <ShipSupportMoney />
+      <TwoBlogsPart />
+      <NaturalQualityCashback />
+      <FaceEyesLips />
+      <Subscribe />
+      <News />
+      <Brands />
+      <InstagramAndImages />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
