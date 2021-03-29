3893959d856433f2f5a188e0df0c11f617103435
Halık Ali TUNÇ <tunchalikali@gmail.com>
2021-03-29 20:48:31 +0300

    ad filter area and product area

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,9 +1,21 @@
+import Filter from "../components/ShopPage/Filter/Filter";
 import Product from "../components/ShopPage/Product";
 
 const Shop = () => {
   return (
     <>
-      <Product />
+      {" "}
+      <div style={{ backgroundColor: "red", height: "500px" }}></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <Filter />
+          </div>
+          <div className="col-md-9">
+            <Product />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/ShopPage/Product.js
+++ src/components/ShopPage/Product.js
@@ -4,32 +4,23 @@ import "./style.scss";
 const Product = () => {
   const products = useSelector((state) => state.productsReducer.data);
   return (
-    <div className="product-section">
-      <div style={{ backgroundColor: "red", height: "500px" }}></div>
-      <div className="container-fluid">
-        <div className="row  ">
-          <div className="col-md-3" style={{ backgroundColor: "red" }}></div>
-          <div className="col-md-9">
-            {products &&
-              products.map((product) => (
-                <div className="card  col-md-4">
-                  <img
-                    className="card-img-top"
-                    src={product.image_link}
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.description}</p>
-                    <p className="card-text">
-                      <span>{product.price_sign}</span> {product.price}{" "}
-                    </p>
-                  </div>
-                </div>
-              ))}{" "}
+    <div className="row">
+      {products &&
+        products.map((product) => (
+          <div className="card col-md-4">
+            <img
+              className="card-img-top"
+              src={product.image_link}
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{product.name}</h5>
+              <p className="card-text">
+                <span>{product.price_sign}</span> {product.price}{" "}
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
+        ))}{" "}
     </div>
   );
 };
