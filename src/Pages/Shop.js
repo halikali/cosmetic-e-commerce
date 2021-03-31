85a09237ce96300885e850275167663578a5c1f2
Halık Ali TUNÇ <tunchalikali@gmail.com>
2021-03-31 22:47:23 +0300

    added filter slider , topComponent

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Filter from "../components/ShopPage/Filter/Filter";
 import Product from "../components/ShopPage/Product/Product";
-
+import TopComponent from "../components/TopComponent/TopComponent";
 const Shop = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -10,7 +10,8 @@ const Shop = () => {
   return (
     <>
       {" "}
-      <div style={{ backgroundColor: "red", height: "500px" }}></div>
+      {/* <div style={{ backgroundColor: "red", height: "500px" }}></div> */}
+      <TopComponent text="SHOP" />
       <div className="container">
         <div className="row">
           <div className="col-md-3">
--- src/components/ShopPage/Filter/Filter.js
+++ src/components/ShopPage/Filter/Filter.js
@@ -2,8 +2,19 @@ import { Range } from "rc-slider";
 import ProductTypes from "../ProductTypes/ProductTypes";
 import "./Filter.scss";
 import "rc-slider/assets/index.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getProductsForPriceFilter } from "../../../store/actions/actions";
 
 const Filter = () => {
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const dispatch = useDispatch();
+  const onChangeHandler = (event) => {
+    setFirstValue(event[0]);
+    setSecondValue(event[1]);
+    dispatch(getProductsForPriceFilter(firstValue, secondValue));
+  };
   return (
     <div className="container bg-white mt-4">
       <section className="filter-bar">
@@ -12,8 +23,9 @@ const Filter = () => {
         <section className="price mb-4">
           <h6 className="font-weight-bold mb-3 text-left">Price</h6>
           <form>
-            <Range />
+            <Range min={0} max={100} onChange={onChangeHandler} />
           </form>
+          {firstValue} - {secondValue}
         </section>
       </section>
     </div>
